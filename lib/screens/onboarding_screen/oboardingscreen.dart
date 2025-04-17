35d6ef6b874eb3381d0a82034d66ed7b6bf98fa0
aryajoshi022 <aryajoshi022@gmail.com>
2025-04-17 10:36:39 +0530

    setup

---
+++ lib/screens/onboarding_screen/oboardingscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class onBoardingScreen extends StatefulWidget {
+  const onBoardingScreen({super.key});
+
+  @override
+  State<onBoardingScreen> createState() => _onBoardingScreenState();
+}
+
+class _onBoardingScreenState extends State<onBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
