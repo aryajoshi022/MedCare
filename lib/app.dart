bef3c3e31ea8f6f9f437a4f44ad87893eb74fe02
Jatin Patel <jatinpatel3803@gmail.com>
2025-04-17 13:19:51 +0530

    Commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:medcare/screens/home/home_screen.dart';
-import 'package:medcare/splashscreen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
