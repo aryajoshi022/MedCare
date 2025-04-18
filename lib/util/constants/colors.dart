842f16215044c200d93ddfcd89ffbf7b4a1f5f4d
Jatin Patel <jatinpatel3803@gmail.com>
2025-04-18 12:53:04 +0530

    Add Bottom App Bar

--- lib/util/constants/colors.dart
+++ lib/util/constants/colors.dart
@@ -4,29 +4,29 @@ class AppColors {
   AppColors._();
 
   // Background Colors
-  static const Color bgPrimary = Color(0xFF161616);
-  static const Color bgAlert = Color(0xFF333333);
+  static const Color bgPrimary = Color(0xFF26408B);
+  static const Color bgAlert = Color(0xFFFFFFFF);
 
 
   //  Text Colors
-  static const Color textPrimary = Color(0xFFFFFFFF);
-  static const Color textSecondary = Color(0xFFB0B0B0);
+  static const Color textPrimary = Color(0xFF121212);
+  static const Color textSecondary = Color(0xFF4D4D4D);
   static const Color textBrand = Color(0xFFB4B2FF);
-  static const Color textBtn = Color(0xFF262626);
-  static const Color textDisabled = Color(0xB20C0C0D);
-  static const Color textError = Color(0xFFFF9999);
+  static const Color textBtn = Color(0xFF26408B);
+  static const Color textDisabled = Color(0xff8F8F8F);
+  static const Color textError = Color(0xFFDCFFDD);
 
 
   // Button Colors
-  static const Color btnPrimary = Color(0xFFA099FF);
-  static const Color btnGrey = Color(0xFF333333);
-  static const Color btnInputField = Color(0xFF262626);
+  static const Color btnPrimary = Color(0xFF26408B);
+  static const Color btnGrey = Color(0xFFFFFFFF);
+  static const Color btnInputField = Color(0xFFF9F9F9);
   static const Color btnDisabled = Color(0xFF5B578A);
   static const Color btnSuccess = Color(0xFF36F279);
 
   // Stroke Colors
   static const Color borderPrimary = Color(0xFF4F4F4F);
-  static const Color borderSecondary = Color(0xFF454545);
+  static const Color borderSecondary = Color(0xFFA6CFD5);
   static const Color borderError = Color(0xFFFD6C6C);
   static const Color borderBrand = Color(0xFFA099FF);
   static const Color borderWhite = Color(0xFFFFFFFF);
