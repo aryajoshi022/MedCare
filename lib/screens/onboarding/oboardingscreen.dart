37927c1b7a90c73b335db4a56760eff0d0ff7d64
aryajoshi022 <aryajoshi022@gmail.com>
2025-04-18 11:04:19 +0530

    onboardingscreen

--- lib/screens/onboarding/oboardingscreen.dart
+++ lib/screens/onboarding/oboardingscreen.dart
@@ -10,8 +10,37 @@ class onBoardingScreen extends StatefulWidget {
 class _onBoardingScreenState extends State<onBoardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('OnBoardingScreen'),
-    );
+    return Scaffold(body: SafeArea(child: Text('ONboarding SCreen')));
   }
+}
+
+class OnboardingContent {
+  final String title, image, description;
+
+  List<OnboardingContent> contents = [
+    OnboardingContent(
+      title: 'Online Consultation',
+      image: 'assets/images/onboard_layer1.png',
+      description:
+          "Connect with healthcare professionals virtually for convenient medical advice and support.",
+    ),
+    OnboardingContent(
+      title: '24 Hours Ready to Serve',
+      image: 'assets/images/onboard_layer1.png',
+      description:
+          "Instant access to expert medical assistance. Get the care you need, when you need it, with our app.",
+    ),
+    OnboardingContent(
+      title: 'Medical Record Data Patient',
+      image: 'assets/images/onboard_layer1.png',
+      description:
+          " Easily manage and access comprehensive health records, including medical history, test results, and treatment plans, all in one secure place.",
+    ),
+  ];
+
+  OnboardingContent({
+    required this.title,
+    required this.image,
+    required this.description,
+  });
 }
