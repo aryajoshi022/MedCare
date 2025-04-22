6f1b899f2b618fde312f4063af295804658a9c9f
RIDDHIPATEL31 <riddhi31504@gmail.com>
2025-04-22 10:42:01 +0530

    doctor list

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:medcare/screens/chatdoctor/chat_doctor.dart';
 import 'package:medcare/screens/chatdoctor/doctor_details.dart';
 
 class App extends StatelessWidget {
