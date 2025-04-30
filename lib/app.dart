ad0d32789433911885c6bdea29d7dd6fefd71352
aryajoshi022 <aryajoshi022@gmail.com>
2025-04-30 10:53:21 +0530

    health_shop

--- lib/app.dart
+++ lib/app.dart
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(428,926), // Match Figma design
+      designSize: const Size(428, 926), // Match Figma design
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
@@ -21,11 +21,11 @@ class App extends StatelessWidget {
           themeMode: ThemeMode.light,
           theme: AppTheme.lighttheme,
           debugShowCheckedModeBanner: false,
-         home: const HomeScreen(),
+          //const HomeScreen(),
           // home: SplashScreen(),
           // home: ChatDoctor(),
           // home: DoctorDetails(),
-          //  home: HealthShopScreen(),
+          home: HealthShopScreen(),
           routes: {},
         );
       },
