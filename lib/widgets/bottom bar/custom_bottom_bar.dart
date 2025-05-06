4a312f17120b89f3fd46874ab15945ca3e2aaf4d
RIDDHIPATEL31 <riddhi31504@gmail.com>
2025-05-06 13:38:41 +0530

    history

--- lib/widgets/bottom bar/custom_bottom_bar.dart
+++ lib/widgets/bottom bar/custom_bottom_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:medcare/screens/home/home_screen.dart';
 
-import '../../screens/chatdoctor/chat_doctor.dart';
 import '../../screens/chatdoctor/history_screen.dart';
 import '../../screens/services/services_screen.dart';
 
