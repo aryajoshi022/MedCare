bf216326be76ae87f1af4d2e2110bc05d620d0f3
RIDDHIPATEL31 <riddhi31504@gmail.com>
2025-05-19 13:50:59 +0530

    profile

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:medcare/screens/MedicationReminder/medication_reminder.dart';
 import 'package:medcare/screens/home/home_screen.dart';
-import 'package:medcare/screens/profile/prescription_history.dart';
 import 'package:medcare/splashscreen.dart';
 import 'package:medcare/util/theme/theme.dart';
 
@@ -21,7 +20,7 @@ class App extends StatelessWidget {
           themeMode: ThemeMode.light,
           theme: AppTheme.lighttheme,
           debugShowCheckedModeBanner: false,
-          home: PrescriptionHistory(),
+          home: const HomeScreen(),
           // home: SplashScreen(),
           // home: ChatDoctor(),
           // home: DoctorDetails(),
