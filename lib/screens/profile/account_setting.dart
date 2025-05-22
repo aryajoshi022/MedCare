b91409a6b2a5b52601623a6259d86eb0b1ba906b
RIDDHIPATEL31 <riddhi31504@gmail.com>
2025-05-22 10:09:27 +0530

    profile

---
+++ lib/screens/profile/account_setting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountSetting extends StatefulWidget {
+  const AccountSetting({super.key});
+
+  @override
+  State<AccountSetting> createState() => _AccountSettingState();
+}
+
+class _AccountSettingState extends State<AccountSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
