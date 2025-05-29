7d3cd5995b0ca4a9fa6bedd44111cadb0b47c527
aryajoshi022 <aryajoshi022@gmail.com>
2025-05-29 12:23:03 +0530

    change

--- lib/screens/signup/signupscreen.dart
+++ lib/screens/signup/signupscreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:medcare/screens/home/home_screen.dart';
 import 'package:medcare/screens/signin/signinscreen.dart';
 import 'package:medcare/util/constants/colors.dart';
